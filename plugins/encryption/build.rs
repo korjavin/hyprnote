ddf63a048d3525579993d6a01dfa0a49078bbaed
Korzhavin Ivan <korjavin@gmail.com>
2025-05-01 12:33:47 +0200

    fix: Update tauri_specta export method to use the correct namespace

--- plugins/encryption/build.rs
+++ plugins/encryption/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     tauri_build::build();
-    tauri_specta::build::export()
+    tauri_specta::export()
         .commands_glob("src/commands.rs")
         .typescript_path("js/bindings.gen.ts")
         .run();
